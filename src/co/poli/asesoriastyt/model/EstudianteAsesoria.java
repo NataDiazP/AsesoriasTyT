143ca29f1a5311fbb2ee9df9d5a6270d8556930a
natad <julydiazp@hotmail.com>
2015-03-14 22:22:39 -0500

    Correcciones varias a formularios

---
+++ src/co/poli/asesoriastyt/model/EstudianteAsesoria.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package co.poli.asesoriastyt.model;
+
+/**
+ * @author natad_000
+ *
+ */
+public class EstudianteAsesoria {
+
+	 private String numDocEstudiante;
+	 private String idAsesoria;
+	 private String asistenciaAsesoria;
+	 
+	/**
+	 * @return the idAsesoria
+	 */
+	public String getIdAsesoria() {
+		return idAsesoria;
+	}
+	/**
+	 * @param idAsesoria the idAsesoria to set
+	 */
+	public void setIdAsesoria(String idAsesoria) {
+		this.idAsesoria = idAsesoria;
+	}
+	/**
+	 * @return the numDocEstudiante
+	 */
+	public String getNumDocEstudiante() {
+		return numDocEstudiante;
+	}
+	/**
+	 * @param numDocEstudiante the numDocEstudiante to set
+	 */
+	public void setNumDocEstudiante(String numDocEstudiante) {
+		this.numDocEstudiante = numDocEstudiante;
+	}
+	/**
+	 * @return the asistenciaAsesoria
+	 */
+	public String getAsistenciaAsesoria() {
+		return asistenciaAsesoria;
+	}
+	/**
+	 * @param asistenciaAsesoria the asistenciaAsesoria to set
+	 */
+	public void setAsistenciaAsesoria(String asistenciaAsesoria) {
+		this.asistenciaAsesoria = asistenciaAsesoria;
+	}
+}
