5ae00418fcb7f4b44e5924bd3d1ac7562a5ec3e3
asesoriastytpaola <paolavl3@hotmail.com>
2015-04-01 08:51:28 -0500

    Se actualiza comentarios de código

--- src/co/poli/asesoriastyt/model/ReporteNumAsistencia.java
+++ src/co/poli/asesoriastyt/model/ReporteNumAsistencia.java
@@ -4,92 +4,92 @@
 package co.poli.asesoriastyt.model;
 
 /**
- * The Class ReporteNumAsistencia.
+ *  Class ReporteNumAsistencia.
  *
  * @author Paola
  */
 public class ReporteNumAsistencia 
 {
 	
-	/** The id_ asesoria. */
+	/**  id_ asesoria. */
 	private String id_Asesoria;
 	
-	/** The nombre_ asesoria. */
+	/**  nombre_ asesoria. */
 	private String nombre_Asesoria;
 	
-	/** The fecha_ asesoria. */
+	/**  fecha_ asesoria. */
 	private String fecha_Asesoria;
 	
-	/** The cantidad. */
+	/**  cantidad. */
 	private String cantidad;
 	
 	/**
-	 * Gets the id_ asesoria.
+	 * Gets  id_ asesoria.
 	 *
-	 * @return the id_Asesoria
+	 * @return  id_Asesoria
 	 */
 	public String getId_Asesoria() {
 		return id_Asesoria;
 	}
 	
 	/**
-	 * Sets the id_ asesoria.
+	 * Sets  id_ asesoria.
 	 *
-	 * @param id_Asesoria the id_Asesoria to set
+	 * @param id_Asesoria  id_Asesoria to set
 	 */
 	public void setId_Asesoria(String id_Asesoria) {
 		this.id_Asesoria = id_Asesoria;
 	}
 	
 	/**
-	 * Gets the nombre_ asesoria.
+	 * Gets  nombre_ asesoria.
 	 *
-	 * @return the nombre_Asesoria
+	 * @return  nombre_Asesoria
 	 */
 	public String getNombre_Asesoria() {
 		return nombre_Asesoria;
 	}
 	
 	/**
-	 * Sets the nombre_ asesoria.
+	 * Sets  nombre_ asesoria.
 	 *
-	 * @param nombre_Asesoria the nombre_Asesoria to set
+	 * @param nombre_Asesoria  nombre_Asesoria to set
 	 */
 	public void setNombre_Asesoria(String nombre_Asesoria) {
 		this.nombre_Asesoria = nombre_Asesoria;
 	}
 	
 	/**
-	 * Gets the fecha_ asesoria.
+	 * Gets  fecha_ asesoria.
 	 *
-	 * @return the fecha_Asesoria
+	 * @return  fecha_Asesoria
 	 */
 	public String getFecha_Asesoria() {
 		return fecha_Asesoria;
 	}
 	
 	/**
-	 * Sets the fecha_ asesoria.
+	 * Sets  fecha_ asesoria.
 	 *
-	 * @param fecha_Asesoria the fecha_Asesoria to set
+	 * @param fecha_Asesoria  fecha_Asesoria to set
 	 */
 	public void setFecha_Asesoria(String fecha_Asesoria) {
 		this.fecha_Asesoria = fecha_Asesoria;
 	}
 	
 	/**
-	 * Gets the cantidad.
+	 * Gets  cantidad.
 	 *
-	 * @return the cantidad
+	 * @return  cantidad
 	 */
 	public String getCantidad() {
 		return cantidad;
 	}
 	
 	/**
-	 * Sets the cantidad.
+	 * Sets  cantidad.
 	 *
-	 * @param cantidad the cantidad to set
+	 * @param cantidad  cantidad to set
 	 */
 	public void setCantidad(String cantidad) {
 		this.cantidad = cantidad;
